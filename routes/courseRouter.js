e46516953f7cf5725daca55208bd59834e306c77
omkaruttirnaservice <omkaruttirnaservice@gmail.com>
2025-08-21 11:52:45 +0530

    ref

--- routes/courseRouter.js
+++ routes/courseRouter.js
@@ -1,28 +1,31 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 const {
-  createCourse,
-  getAllCourses,
-  getAllCourse,
-  getCourseById,
-  updateCourse,
-  deleteCourse,
-} = require("../controllers/courseController");
+    createCourse,
+    getAllCourses,
+    getAllCourse,
+    getCourseById,
+    updateCourse,
+    deleteCourse,
+} = require('../controllers/courseController');
 
 // POST /api/courses
-router.post("/", createCourse);
+router.post('/', createCourse);
 
 // GET /api/courses
-router.get("/", getAllCourses);
-router.get("/all", getAllCourse);
-// GET /api/courses/:id
-router.get("/:id", getCourseById);
+router.get('/', getAllCourses);
+
+// GET /api/courses/all
+router.get('/all', getAllCourse);
+
+// GET /api/courses/:id (only matches valid MongoDB ObjectId)
+router.get('/:id([0-9a-fA-F]{24})', getCourseById);
 
 // PUT /api/courses/:id
-router.put("/:id", updateCourse);
+router.put('/:id([0-9a-fA-F]{24})', updateCourse);
 
 // DELETE /api/courses/:id
-router.delete("/:id", deleteCourse);
+router.delete('/:id([0-9a-fA-F]{24})', deleteCourse);
 
 module.exports = router;
